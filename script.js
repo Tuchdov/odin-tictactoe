ec4ea1e3c9ec41cea7e5837e75b860924ad930c4
Tuchdov <tuchdov@gmail.com>
2025-10-07 16:08:09 +0300

    Add winning on row conditions

--- script.js
+++ script.js
@@ -52,19 +52,50 @@ const gameController = (function () {
     const secondUserName = prompt('Enter the name of the second player');
     const playerOne = Player(firstUserName, 'X');
     const playerTwo = Player(secondUserName, 'O');
-    const Players = [
+    const players = [
         playerOne ,
         playerTwo
     ]
 
-    let activePlayer = Players[0];
+    let activePlayer = players[0];
 
     const switchPlayerTurn = ()=>{
-        activePlayer= activePlayer === Players[0] ? Players[1] : Players[0];
+        activePlayer= activePlayer === players[0] ? players[1] : players[0];
     }
 
     const playRound = (row,col) => {
         console.log(`It's ${activePlayer.name}'s turn`);
         Gameboard.placeMark(row -1, col- 1, activePlayer.token);
+    }
+
+    const  checkRowWin = ()=> {
+        let boardState = Gameboard.getBoard();
+        const dimensions = 3;
+
+        for (let i = 0; i < dimensions ; i ++){
+            let playerOneCounter = 0;
+            let playerTwoCounter = 0;
+            for (let j = 0; j < dimensions; j++){
+                cell = boardState[i][j];
+                if(cell ===  players[0].token){
+                    playerOneCounter++;
+                    playerTwoCounter = 0;
+                    }
+              else if(cell === players[1].token){
+                playerOneCounter =0;
+                playerTwoCounter++;
+            }
+            else {
+                playerOneCounter = 0;
+                playerTwoCounter = 0;
+            }
+            }
+            if (playerOneCounter === 3 || playerTwoCounter === 3) {
+                console.log(`${activePlayer.name} has won the game on a row!`);
+                return true;
+            }
+
+         }
+         return false;
     }
 })();
